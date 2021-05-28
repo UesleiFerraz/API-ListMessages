f8bcd9c4d8ee66ef7505e8a270449ecfa638d2c2
Ueslei <uesleivaleriod@gmail.com>
2021-05-28 17:14:36 -0300

    small update routes

--- src/routes.ts
+++ src/routes.ts
@@ -9,11 +9,11 @@ routes.get("/users/:userId", UserController.listOneUser);
 routes.get("/users/:userId/scraps", ScrapController.listAllScrapsUser);
 routes.get("/users/:userId/scraps/:scrapId", ScrapController.listOneScrapUser);
 
-routes.post("/users/:userId/scraps", ScrapController.addScrap);
 routes.post("/users", UserController.addUser);
+routes.post("/users/:userId/scraps", ScrapController.addScrap);
 
-routes.put("/users/:userId/scraps/:scrapId", ScrapController.updateScrap);
 routes.put("/users/:userId", UserController.updateUser);
+routes.put("/users/:userId/scraps/:scrapId", ScrapController.updateScrap);
 
 routes.delete("/users/:userId", UserController.deleteUser);
 routes.delete("/users/:userId/scraps/:scrapId", ScrapController.deleteScrap);
