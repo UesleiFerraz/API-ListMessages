18359a289dc020cefc73619da8a4ac086a87c0c2
Ueslei <uesleivaleriod@gmail.com>
2021-05-28 15:57:21 -0300

    update routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import UserController from "./controllers/UserController";
 
 const routes = Router();
 
-routes.get("/", (req, res) => {
-  res.send("hello");
-});
+routes.get("/", UserController.listAllUsers)
+routes.get("/users/:id", UserController.listOneUser)
+routes.post('/users', UserController.addUser)
+routes.delete('/users/:id', UserController.deleteUser)
+routes.put('/users/:id', UserController.updateUser)
 
 export default routes;
