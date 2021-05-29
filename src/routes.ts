799c74582768d8aad2bff8883d6f9f216a961b7e
Ueslei <uesleivaleriod@gmail.com>
2021-05-29 12:28:25 -0300

    small update routes

--- src/routes.ts
+++ src/routes.ts
@@ -7,18 +7,18 @@ import userHasSameId from "./middlewares/userHasSameId";
 const routes = Router();
 
 routes.get("/", userAuth, UserController.listAllUsers);
-routes.get("/users/:userId", userAuth, UserController.listOneUser);
+routes.get("/users/:userId", [userAuth, userHasSameId], UserController.listOneUser);
 routes.get("/users/:userId/scraps", [userAuth, userHasSameId], ScrapController.listAllScrapsUser);
 routes.get("/users/:userId/scraps/:scrapId", userAuth, ScrapController.listOneScrapUser);
 
 routes.post("/users", UserController.addUser);
 routes.post("/auth", UserController.login);
-routes.post("/users/:userId/scraps", userAuth, ScrapController.addScrap);
+routes.post("/users/:userId/scraps", [userAuth, userHasSameId], ScrapController.addScrap);
 
-routes.put("/users/:userId", userAuth, UserController.updateUser);
-routes.put("/users/:userId/scraps/:scrapId", userAuth, ScrapController.updateScrap);
+routes.put("/users/:userId", [userAuth, userHasSameId], UserController.updateUser);
+routes.put("/users/:userId/scraps/:scrapId", [userAuth, userHasSameId], ScrapController.updateScrap);
 
-routes.delete("/users/:userId", userAuth, UserController.deleteUser);
-routes.delete("/users/:userId/scraps/:scrapId", userAuth, ScrapController.deleteScrap);
+routes.delete("/users/:userId", [userAuth, userHasSameId], UserController.deleteUser);
+routes.delete("/users/:userId/scraps/:scrapId", [userAuth, userHasSameId], ScrapController.deleteScrap);
 
 export default routes;
